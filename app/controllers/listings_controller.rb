a1ea05bd2a3a5933aec73405f32a2840768d12e6
田所拓 <beherzt.e0102@gmail.com>
2017-01-09 15:10:58 +0900

    secret key changed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,25 +1,29 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_listing, omly: [:update, :basics, :description, :address, :price, :photos, :calendar, :bankaccount]
+  before_action :set_listing, only: [:update, :basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
 
   def index
+    @listings = current_user.listings
   end
 
   def show
   end
 
   def new
+    # 現在のユーザーのリスティングの作成
     @listing = current_user.listings.build
   end
 
   def create
+    # パラメーターとともに現在のユーザーのリスティングを作成
     @listing = current_user.listings.build(listing_params)
 
     if @listing.save
-      redirect_to manage_listing_basics_path(@listing), notice: "リスティングを作成・保存しました"
+      redirect_to manage_listing_basics_path(@listing), notice: "リスティングを作成・保存をしました"
     else
-      redirect_to new_listing_path(@listing), notice: "リスティングを作成・保存できませんでした"
+      redirect_to new_listing_path, notice: "リスティングを作成・保存出来ませんでした"
     end
+
   end
 
   def edit
@@ -56,6 +60,8 @@ class ListingsController < ApplicationController
   def publish
   end
 
+
+
   private
   def listing_params
     params.require(:listing).permit(:home_type, :pet_type, :breeding_years, :pet_size, :price_per_night)
@@ -64,5 +70,6 @@ class ListingsController < ApplicationController
   def set_listing
     @listing = Listing.find(params[:id])
   end
+
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.paperclip_defaults = {
   storage: :s3,
   s3_protocol: :https,
   s3_credentials: {
